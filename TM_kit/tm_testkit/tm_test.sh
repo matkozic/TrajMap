83b576013bdaa9ef067777a0c5485521f6524a83
matkozic <109069239+matkozic@users.noreply.github.com>
2023-12-23 13:57:55 +0100

    Update tm_test.sh

--- TM_kit/tm_testkit/tm_test.sh
+++ TM_kit/tm_testkit/tm_test.sh
@@ -9,19 +9,19 @@ testpdb.pdb
 1
 testpdb.pdb
 testcsv.csv
-249
+60
 2
 testcsv.csv
 3
 testfig.png
 testtitle
-25,5,10,1,0,7,249,0,0
+25,5,10,1,0,7,60,0,0
 4
-170,175,0,49
+44,54,0,50
 5
 testgraph.png
 testgraph
-residues 170-175
+residues 44-54
 0,10,1,0.1
 0,50,5,1
 5
@@ -30,7 +30,7 @@ h
 q
 q
 INPUTS
-#rm testpdb.pdb testcsv.csv 
-rm testpdb.pdb testcsv.csv testfig.png testgraph.png
+ 
+#rm testpdb.pdb testcsv.csv testfig.png testgraph.png
 echo If it exited without errors it probably works fine.
 
